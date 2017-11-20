47d08fce7a641f66f08cc47b9db81e7f153b78f4
yardencsGitHub <yardenc@bu.edu>
2017-11-20 13:31:52 -0500

    Update normalization

--- helpers/HelpPrepareBGlessMovies.m
+++ helpers/HelpPrepareBGlessMovies.m
@@ -1,14 +1,15 @@
 %% To prepare BG subtracted movies ...
-% The movie Y isa 3d matrix rows x columns x frames
+% The movie Y is a 3d matrix rows x columns x frames
 % code doesn't deal with dropped frames and \ or motion correction (frame
 % registration)
-n_del_frames = 5; % How many frames to disregard at the video's onset
+n_del_frames = 6; % How many frames to disregard at the video's onset
 [rows,columns,frames] = size(Y);
 
 % calculate a single background frame - b1
 b1 = quantile(Y(:,:,n_del_frames+1:end),0.1,3);
-b1 = (b1-min(b1(:)))/max(b1(:));
-b1 = b1/median(b1(:));
+b1 = (b1-min(b1(:)))/(max(b1(:))-min(b1(:)));
+% legacy .... b1 = b1/median(b1(:));
+
 % calculate a backroung time series as the median of each frame
 bt = median(reshape(Y,rows*columns,frames));
 % create a BG movie by scaling the BG frame (b1) by the BG time series (bt) and get b2, a very meaningful name. 
@@ -21,7 +22,7 @@ c = Y-b2;
 c = c - min(min(min(c(:,:,n_del_frames+1:end))));
 %% optional, crop first 'n_del_frames' frames and normalize
 c1 = c(:,:,n_del_frames:end);
-c1 = c1/max(c1(:))*255;
+%c1 = c1/max(c1(:))*255;
 
 %% calculate df/f using each pixel's 5% quantile as baseline
 dff_temp = bsxfun(@rdivide,bsxfun(@minus,c1,quantile(c1,0.05,3)),quantile(c1,0.05,3));
